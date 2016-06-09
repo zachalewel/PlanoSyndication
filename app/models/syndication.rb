1b179f55ad4f9dcdc9ae7c2a351ad59b86b84498
Zach Alewel <zachalewel@gmail.com>
2016-06-09 15:38:05 -0600

    adds class methods to syndication model

--- app/models/syndication.rb
+++ app/models/syndication.rb
@@ -1,3 +1,10 @@
 class Syndication < ActiveRecord::Base
 
+  def self.find_tour_urls
+    where(name: 'tour_urls.txt').order(created_at: :desc)
+  end
+
+  def self.find_tour_slides
+    where(name: 'tour_slides_descriptions.txt').order(created_at: :desc)
+  end
 end
